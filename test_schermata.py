1038fc41233de16a446b2ac53fd17350ced9e131
mattabott <mattabott@gmail.com>
2024-01-24 17:06:29 +0000

    added new functions

--- test_schermata.py
+++ test_schermata.py
@@ -7,24 +7,25 @@ gpio.setwarnings(False)
 gpio.setmode(gpio.BOARD)
 gpio.setup(10, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
-PASSWORD_CORRETTA = "segreta"  # Imposta la tua password qui
+PASSWORD_CORRETTA = "password"  # Imposta la tua password qui
 
 def start_countdown():
     global counter, running
-    counter = 10  # Durata del countdown in secondi
+    counter = 600  # Durata del countdown in secondi
     running = True
     update_label()
 
 def update_label():
     global counter
     if counter > 0 and running:
-        label.config(text=str(counter))
+        minutes, seconds = divmod(counter, 60)
+        label.config(text=f"{minutes:02d}:{seconds:02d}")
         counter -= 1
         window.after(1000, update_label)
     elif not running:
-        label.config(text="Ferma")
+        label.config(text=counter)
     else:
-        label.config(text="Finito!")
+        label.config(text=str(counter))
 
 def stop_countdown():
     global running
@@ -34,7 +35,8 @@ def verifica_password():
     password = simpledialog.askstring("Password", "Inserisci la password:", parent=window, show='*')
     if password == PASSWORD_CORRETTA:
         global counter
-        counter = 0
+        global running
+        counter = counter
         running = False
     else:
         label.config(text="Password Errata")
@@ -51,7 +53,7 @@ label.pack(pady=40)
 start_button = tk.Button(window, text="Inizia", font=("Arial", 20), command=start_countdown, bg="green", fg="white", height=2, width=10)
 start_button.pack(side=tk.LEFT, padx=50)
 
-stop_button = tk.Button(window, text="Ferma", font=("Arial", 20), command=stop_countdown, bg="red", fg="white", height=2, width=10)
+stop_button = tk.Button(window, text="STOP", font=("Arial", 20), command=stop_countdown, bg="red", fg="white", height=2, width=10)
 stop_button.pack(side=tk.RIGHT, padx=50)
 
 counter = 0
@@ -62,5 +64,6 @@ while True:
 	if gpio.input(10) == gpio.HIGH:
 		print("Button was pushed")
 		break
+start_countdown()
 
 window.mainloop()
