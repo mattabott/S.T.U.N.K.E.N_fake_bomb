d2d13666a1a700a2e11cba6fc64b15b920c8641f
mattabott <mattabott@gmail.com>
2024-02-07 17:07:02 +0000

    Added button to simulate HD attachment

--- pico/code.py
+++ pico/code.py
@@ -6,37 +6,73 @@ import board
 import digitalio
 import time
 
-red = digitalio.DigitalInOut(board.GP5)
-red.direction = digitalio.Direction.OUTPUT
-red.value = True
-
-green = digitalio.DigitalInOut(board.GP13)
-green.direction = digitalio.Direction.OUTPUT
-green.value = False
+# Definizione della classe di eccezione
+class ButtonReleasedException(Exception):
+    pass
 
 kbd = Keyboard(usb_hid.devices)
 keyboard_layout = KeyboardLayoutUS(kbd)
-# Press ctrl-x.
-kbd.press(Keycode.LEFT_CONTROL, Keycode.LEFT_ALT, Keycode.T)
-time.sleep(0.1)
-kbd.release_all()
 
-time.sleep(1)
+red = digitalio.DigitalInOut(board.GP5)
+red.direction = digitalio.Direction.OUTPUT
 
-keyboard_layout.write("cd /home/mattabott/Documents/G/D/I/S.T.U.N.K.E.N_fake_bomb")
-kbd.press(Keycode.ENTER)
-kbd.release_all()
-time.sleep(0.5)
+green = digitalio.DigitalInOut(board.GP13)
+green.direction = digitalio.Direction.OUTPUT
 
-keyboard_layout.write("python3 secret.py")
-kbd.press(Keycode.ENTER)
-kbd.release_all()
-time.sleep(0.5)
+btn = digitalio.DigitalInOut(board.GP16)
+btn.switch_to_input(pull=digitalio.Pull.UP)
 
-time.sleep(5)
+def btn_on():
+    if btn.value == False:
+        green.value = True
+        red.value = False
+        return True
+    else:
+        green.value = False
+        red.value = True
+        return False
 
-green.value = True
-red.value = False
+def check_on():
+    if not btn_on():
+        kbd.press(Keycode.LEFT_CONTROL, Keycode.LEFT_SHIFT, Keycode.W)
+        time.sleep(0.1)
+        kbd.release_all()
+        raise ButtonReleasedException
 
-while True:
-    pass
+def execute_commands(sleep_time):
+    try:
+        kbd.press(Keycode.LEFT_CONTROL, Keycode.LEFT_ALT, Keycode.T)
+        time.sleep(0.1)
+        kbd.release_all()
+        time.sleep(1)
+        check_on()
+        
+        keyboard_layout.write("cd /home/mattabott/Documents/G/D/I/S.T.U.N.K.E.N_fake_bomb")
+        kbd.press(Keycode.ENTER)
+        kbd.release_all()
+        time.sleep(0.5)
+        check_on()
+        keyboard_layout.write("python3 secret.py")
+        kbd.press(Keycode.ENTER)
+        kbd.release_all()
+        
+        for i in range(sleep_time*4):
+            time.sleep(i/4)
+            check_on()
+        
+    except ButtonReleasedException:
+        print("Pulsante rilasciato durante l'esecuzione dei comandi")
+        return  # Uscita dalla funzione
+
+    green.value = True
+    red.value = False
+
+try:
+    while True:
+        if btn_on():
+            execute_commands(5)
+
+        time.sleep(0.1)
+except Exception as e:
+    print(f"Errore: {e}")
+
